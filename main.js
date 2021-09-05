7c0d54211bd1675ca7967e03942b9a9946954bc9
Isabel Servin <Isabel.servin@outlook.com>
2021-09-05 15:47:00 -0700

    generate random price and delivery day

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 //prompt user to enter item and qty
 let greeting = alert("Welcome to the UPS storefront, I will be your virtual assistant!")
-let newItem = prompt("What would you like to order today?")
+let item = prompt("What would you like to order today?")
 let qty = prompt("How many would you like to order?")
 
 //randomly assign a price
-const generatePrice = (min, max) => {
+const generateRandomPrice = (min, max) => {
     let price = (Math.random() * max) + min
     if (price >= max) {
         return price = max.toFixed(2)
@@ -13,4 +13,22 @@ const generatePrice = (min, max) => {
     }
 }
 
-console.log(generatePrice(1, 100))
+//calculate the total cost of order
+const calcTotalPrice =(num1, num2) => {
+    let total = num1 * num2
+    return total.toFixed(2)
+}
+
+//generate rand int between 3-12 days for delivery date
+const genDeliveryDay = (min, max) => {
+    let lengthOfDelivery = Math.round(Math.random() * (max - min + 1) + min)
+    return lengthOfDelivery
+}
+
+let lengthOfDelivery = genDeliveryDay(3,12)
+
+let price = generateRandomPrice(1,100)
+let totalCost = calcTotalPrice(price, qty)
+
+console.log(`Wonderful! You would like to order ${qty} ${item}, costing $${price} each. Your total cost is $${totalCost} and will be deliverd in ${lengthOfDelivery} days.`)
+
